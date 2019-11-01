5365af9e294a0d80d104f40c961997283c7cc603
Juan <juan@realworks.nl>
2019-11-01 14:18:24 +0100

    Fixed react hook and displaying html from string

--- src/components/CoffeeMachine.js
+++ src/components/CoffeeMachine.js
@@ -1,38 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Helmet } from "react-helmet";
 import Container from "react-bootstrap/Container";
 import { CoffeeMachineItem } from "./coffee-machine-item/CoffeeMachineItem";
 
 export const CoffeeMachine = () => {
     let { coffeeSlug } = useParams();
-
     const [machine, setMachine] = useState({});
     const [noData, setNoData] = useState(true);
-    const [helmet, setHelmet] = useState("");
 
     useEffect(() => {
+        const dataUrl = `http://localhost:3000/${coffeeSlug}.json`;
         const fetchData = async () => {
             try {
-                const result = await axios(
-                    `http://localhost:3000/${coffeeSlug}.json`
-                );
+                const result = await axios(dataUrl);
                 setMachine(result.data.machine);
                 setNoData(false);
-                setHelmet(machine.content[0].machine_name);
             } catch (error) {
                 setNoData(true);
             }
         };
         fetchData();
-    }, [coffeeSlug, machine]);
+    }, [coffeeSlug]);
 
     return (
         <>
-            <Helmet>
-                <title>{helmet}</title>
-            </Helmet>
             <Container>
                 {noData ? (
                     <div>No coffee yet...</div>
--- src/components/coffee-machine-item/CoffeeMachineItem.js
+++ src/components/coffee-machine-item/CoffeeMachineItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -12,31 +13,39 @@ export const CoffeeMachineItem = props => {
     const machineContent = machine.content[0];
 
     return (
-        <Container>
-            <Button variant="dark">
-                <Link to={"/"} style={{ color: "#fff" }}>
-                    Back to machines
-                </Link>
-            </Button>
-            <Row style={{ marginTop: "40px" }}>
-                <Col md={4}>
-                    <Image
-                        src={machineContent.machine_gallery[0].thumbUrl}
-                        fluid
-                    />
-                </Col>
-                <Col md={{ span: 6, offset: 2 }}>
-                    <h1 className="machine-title">
-                        {machineContent.machine_name}
-                    </h1>
-                    <h2 className="machine-subtitle">
-                        {machineContent.machine_subline}
-                    </h2>
-                    <p className="machine-description">
-                        {machineContent.machine_description_full}
-                    </p>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            <Helmet>
+                <title>{machineContent.machine_name}</title>
+            </Helmet>
+            <Container>
+                <Button variant="dark">
+                    <Link to={"/"} style={{ color: "#fff" }}>
+                        Back to machines
+                    </Link>
+                </Button>
+                <Row style={{ marginTop: "40px" }}>
+                    <Col md={4}>
+                        <Image
+                            src={machineContent.machine_gallery[0].thumbUrl}
+                            fluid
+                        />
+                    </Col>
+                    <Col md={{ span: 6, offset: 2 }}>
+                        <h1 className="machine-title">
+                            {machineContent.machine_name}
+                        </h1>
+                        <h2 className="machine-subtitle">
+                            {machineContent.machine_subline}
+                        </h2>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: machineContent.machine_description_full
+                            }}
+                            className="machine-description"
+                        ></p>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 };
